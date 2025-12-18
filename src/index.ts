a8d90f5740fff03614017b6723e203884f241eb6
tequ <git@tequ.dev>
2025-12-18 15:01:33 +0900

    initialization

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const URL = "https://build.xahau.tech/"
+
+const getFilenames = async (releaseType: string) => {
+  const response = await fetch(URL)
+  const text = await response.text() as string
+  return (text.match(/>[^<]+</g) || [])
+    .map(filename => filename.replace(/>|</g, ''))
+    .filter(filename => filename.match(/^\S+\+[0-9]{2,4}$/))
+    .filter(filename => filename.includes(releaseType))
+    .sort((a, b) => parseInt(b.split('+')[1]) - parseInt(a.split('+')[1]))
+}
+
+app.get('/', async (c) => {
+  const releaseType = 'release'
+  const filenames = await getFilenames(releaseType)
+  
+  if (filenames.length === 0)
+    return c.json({ error: 'File not found' }, 404)
+  
+  return c.text(filenames[0])
+})
+
+
+app.get('/release/:releaseType?', async (c) => {
+  const releaseType = c.req.param('releaseType') || 'release'
+  const filenames = await getFilenames(releaseType)
+  
+  if (filenames.length === 0)
+    return c.json({ error: 'Invalid release type' }, 400)
+  
+  return c.text(filenames[0])
+})
+
+app.get('/releases/:releaseType?', async (c) => {
+  const releaseType = c.req.param('releaseType') || 'release'
+  const filenames = await getFilenames(releaseType)
+  
+  if (filenames.length === 0)
+    return c.json({ error: 'Invalid release type' }, 400)
+  
+  return c.json(filenames)
+})
+
+export default app
